96ffc760096a9797736030c066db21282471e315
KarthikChillal <chillalkarthik@gmail.com>
2025-12-17 14:06:21 +0530

    commit#2. Made changes to the function gridMaker.

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const container=document.querySelector(".container");
 let gridh=16;
 let gridv=16;
-let valh=100/gridh;
-let valv=100/gridv;
 
-for(let i=0; i<gridh*gridv; i++){
-  container.appendChild(document.createElement("div"));
-}
-const boxes = document.querySelectorAll(".container > div");
-boxes.forEach((ele)=>{
-  ele.style.width=`${valh}%`;
-  ele.style.height=`${valv}%`;
-  ele.style.border=`1px solid black`;
-  ele.addEventListener("mouseover", (e)=>{
-    e.target.style.backgroundColor="hsl(0, 0%, 63%)";
+function gridMaker(h, v){
+  for(let i=0; i<h*v; i++){
+    container.appendChild(document.createElement("div"));
+  }
+  const boxes = document.querySelectorAll(".container > div");
+  boxes.forEach((ele)=>{
+    let valh=100/h;
+    let valv=100/v;
+    ele.style.width=`${valh}%`;
+    ele.style.height=`${valv}%`;
+    // ele.style.border=`1px solid black`;
+    ele.addEventListener("mouseover", (e)=>{
+      e.target.style.backgroundColor="hsla(0, 0%, 0%, 1.00)";
+    });
   });
-});
+}
+gridMaker(16, 16);
